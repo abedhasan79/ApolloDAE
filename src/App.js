ce9b4ee0bfcdf70df09682dda6889edca3e5ad5f
Diana Vu <aznalive@hotmail.com>
2022-11-22 13:17:11 -0500

    installed headless ui icons and vue, testing out if it works. Checks out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    <div>
+    <h1>hello world</h1>
     </div>
   );
 }

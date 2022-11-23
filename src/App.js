f4e82aa56a2ab5c6c8d7afdd0990fe0156fa09fe
Diana Vu <aznalive@hotmail.com>
2022-11-22 19:04:16 -0500

    add light/darkmode function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import { createContext, useState } from 'react'
+import ReactSwitch from 'react-switch';
 
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState('dark');
+  //toggles the theme when it is called
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === 'light' ? 'dark' : 'light'))
+  }
   return (
-    <div>
-    <h1>hello world</h1>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>  
+    <div id={theme}>
+      <h1>helloworld</h1>
+      <div className='switch'>
+        <label> {theme === 'light' ? 'Light Mode' : 'Dark Mode'} </label>
+        <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}/>
+      </div>
     </div>
+    </ThemeContext.Provider>
   );
 }
 

cfa7a1598180d65ef7c942b97ddb4e828d8bd471
Diana Vu <aznalive@hotmail.com>
2022-11-24 18:13:40 -0500

    added darkmode/lightmode

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,9 @@
-import React from "react";
-import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
+import React, { useState } from 'react'
+import ProductList from '../components/ProductList'
+import CategoryMenu from '../components/CategoryMenu'
 import { Banner } from '../components/Banner'
+
+import { FaMoon, FaSun } from 'react-icons/fa'
 
 const style = {
   hw: {
@@ -9,18 +11,35 @@ const style = {
   }
 }
 
-
 const Home = () => {
-  return (
-     <div style={style.hw}>
-      <Banner />
-       <div>
-      <CategoryMenu />
-      <ProductList />
-    </div>
-    </div>
-   
-  );
-};
+  const [darkMode, setDarkMode] = useState(false)
 
-export default Home;
+  return (
+    <div
+      className={
+        darkMode ? 'app-container dark-mode' : 'app-container light-mode'
+      }
+    >
+      <div className='theme-switch'>
+        {!darkMode ? (
+          <span onClick={() => setDarkMode(!darkMode)}>
+            <FaMoon className='fa-icon' />
+          </span>
+        ) : (
+          <span onClick={() => setDarkMode(!darkMode)}>
+            <FaSun className='fa-icon' />
+          </span>
+        )}
+      </div>
+      <div style={style.hw}>
+        <Banner />
+        <div>
+          <CategoryMenu />
+          <ProductList />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home

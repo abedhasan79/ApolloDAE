abe6e97c09eef35fdafb336e773a26020709e145
Diana Vu <aznalive@hotmail.com>
2022-11-25 18:48:36 -0500

    login and sign up now switches

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -31,130 +31,130 @@ export function Signup (props) {
     })
   }
 
-  const [modal, setModal] = useState(false)
-
-  const toggleModal = () => {
-    setModal(!modal)
+  const closeModal = () => {
+    // toggle off sign up modal
+    props.setIsSignUp(false)
+    // toggle off login modal
+    props.setIsLoginModal(false)
   }
 
-  if (modal) {
-    document.body.classList.add('active-modal')
-  } else {
-    document.body.classList.remove('active-modal')
+  const showLogin = e => {
+    e.preventDefault()
+    // toggle off sign up modal
+    props.setIsSignUp(false)
+    // toggle on login modal
+    props.setIsLoginModal(true)
   }
 
   return (
     <>
-      <button onClick={toggleModal}>Signup</button>
-
-      {modal && (
-        <div className='modal'>
-          <div onClick={toggleModal} className='overlay'></div>
-          <div className='modal-content'>
-            <div className='relative flex flex-col justify-center overflow-hidden'>
-              <div className='w-full p-6 m-auto bg-white shadow-md lg:max-w-xl'>
-                <h1 className='text-3xl text-center'>
-                  🔆Apollo<span class='text-blue-400'>D</span>
-                  <span class='text-purple-500'>A</span>
-                  <span class='text-fuchsia-300'>E</span>
-                </h1>
-                <h1 className='text-2xl text-center text-purple-700'>
-                  Create an account
-                </h1>
-                <form onSubmit={handleFormSubmit} className='mt-6'>
-                  <div className='mb-2'>
-                    <label
-                      for='firstname'
-                      className='block text-sm font-semibold text-gray-800'
-                    >
-                      First Name
-                    </label>
-                    <input
-                      placeholder='First'
-                      name='firstName'
-                      type='firstName'
-                       className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
-                      id='firstName'
-                      onChange={handleChange}
-                    />
-                  </div>
-                  <div className='mb-2'>
-                    <label
-                      for='lastName'
-                      className='block text-sm font-semibold text-gray-800'
-                    >
-                      Last Name
-                    </label>
-                    <input
-                      placeholder='Last'
-                      name='lastName'
-                      type='lastName'
-                       className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
-                      id='lastName'
-                      onChange={handleChange}
-                    />
-                  </div>
-                  <div className='mb-2'>
-                    <label
-                      for='email'
-                      className='block text-sm font-semibold text-gray-800'
-                    >
-                      Email
-                    </label>
-                    <input
-                      type='email'
-                      className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
-                      name='email'
-                      onChange={handleChange}
-                      placeholder='Email'
-                    />
-                  </div>
-                  <div className='mb-2'>
-                    <label
-                      for='password'
-                      className='block text-sm font-semibold text-gray-800'
-                    >
-                      Password
-                    </label>
-                    <input
-                      type='password'
-                      className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
-                      name='password'
-                      onChange={handleChange}
-                      placeholder='Password'
-                    />
-                  </div>
-                  <a href='#' className='text-xs text-blue-600 hover:underline'>
-                    Forget Password?
-                  </a>
-                  <div className='mt-6'>
-                    <button
-                      type='submit'
-                      className='w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600'
-                    >
-                      Create Account
-                    </button>
-                  </div>
-                </form>
-
-                <p className='mt-8 text-xs font-light text-center text-gray-700'>
-                  {' '}
-                  Have an account?{' '}
-                  <a
-                    href='#'
-                    className='font-medium text-blue-600 hover:underline'
+      <div className='modal'>
+        <div onClick={closeModal} className='overlay'></div>
+        <div className='modal-content'>
+          <div className='relative flex flex-col justify-center overflow-hidden'>
+            <div className='w-full p-6 m-auto bg-white shadow-md lg:max-w-xl'>
+              <h1 className='text-3xl text-center'>
+                🔆Apollo<span class='text-blue-400'>D</span>
+                <span class='text-purple-500'>A</span>
+                <span class='text-fuchsia-300'>E</span>
+              </h1>
+              <h1 className='text-2xl text-center text-purple-700'>
+                Create an account
+              </h1>
+              <form onSubmit={handleFormSubmit} className='mt-6'>
+                <div className='mb-2'>
+                  <label
+                    for='firstname'
+                    className='block text-sm font-semibold text-gray-800'
                   >
-                    <Login />
-                  </a>
-                </p>
-              </div>
+                    First Name
+                  </label>
+                  <input
+                    placeholder='First'
+                    name='firstName'
+                    type='firstName'
+                    className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
+                    id='firstName'
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className='mb-2'>
+                  <label
+                    for='lastName'
+                    className='block text-sm font-semibold text-gray-800'
+                  >
+                    Last Name
+                  </label>
+                  <input
+                    placeholder='Last'
+                    name='lastName'
+                    type='lastName'
+                    className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
+                    id='lastName'
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className='mb-2'>
+                  <label
+                    for='email'
+                    className='block text-sm font-semibold text-gray-800'
+                  >
+                    Email
+                  </label>
+                  <input
+                    type='email'
+                    className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
+                    name='email'
+                    onChange={handleChange}
+                    placeholder='Email'
+                  />
+                </div>
+                <div className='mb-2'>
+                  <label
+                    for='password'
+                    className='block text-sm font-semibold text-gray-800'
+                  >
+                    Password
+                  </label>
+                  <input
+                    type='password'
+                    className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
+                    name='password'
+                    onChange={handleChange}
+                    placeholder='Password'
+                  />
+                </div>
+                <a href='#' className='text-xs text-blue-600 hover:underline'>
+                  Forget Password?
+                </a>
+                <div className='mt-6'>
+                  <button
+                    type='submit'
+                    className='w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600'
+                  >
+                    Create Account
+                  </button>
+                </div>
+              </form>
+
+              <p className='mt-8 text-xs font-light text-center text-gray-700'>
+                {' '}
+                Have an account?{' '}
+                <a
+                  href='#'
+                  className='font-medium text-blue-600 hover:underline'
+                  onClick={e => showLogin(e)}
+                >
+                  Login
+                </a>
+              </p>
             </div>
-            <button className='close-modal' onClick={toggleModal}>
-              CLOSE
-            </button>
           </div>
+          <button className='close-modal' onClick={closeModal}>
+            CLOSE
+          </button>
         </div>
-      )}
+      </div>
     </>
   )
 }

26f033b2d1e5507ad42c81f9b7c719ccf6c25311
Abed Hasan <abedhasan79@gmail.com>
2022-11-25 23:37:33 -0500

    admin page only works for admin users now

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,28 +1,47 @@
 import React from "react";
 import ProductList from "../components/Admin/ProductList";
 import AddProductFrom from "../components/Admin/AddProductFrom";
-
+import { QUERY_USER } from "../utils/queries";
 import Auth from '../utils/auth';
-
+import { useQuery } from "@apollo/client";
+import spinner from '../assets/spinner.gif'
+import { useStoreContext } from '../utils/GlobalState';
+import { Link } from "react-router-dom";
 const style = {
     hw: {
-      marginTop: '10vh'
+        marginTop: '10vh'
+    },
+
+    hw2:{
+        marginTop: '10vh',
+        marginBottom: '10vh'
     }
-  }
+}
 
 const Admin = () => {
+
+    const { loading, data } = useQuery(QUERY_USER);
     
+    // console.log(data.user.isAdmin);
 
     return (
-        <div className="container" style={style.hw}>
-            welcome to Admin
-            <div>
-                <AddProductFrom/>
-                <ProductList />
-            </div>
+
+        <div className="container" style={style.hw2}>
+            {data && data.user.isAdmin? (
+                <div>
+                    welcome to Admin
+                    <AddProductFrom />
+                    <ProductList />
+                </div>
+            ):(
+                <Link to='/' > YOU NEDD TO LOG IN AS A ADMIN TO USE THIS PAGE. CLICK HERE TO GO BACK TO HOME PAGE.</Link>
+            )}
+            
         </div>
 
     );
+
+
 };
 
 export default Admin;

89e1a22adde3f9eac70530467f2b0e9d25ea2bd8
Abed Hasan <abedhasan79@gmail.com>
2022-11-26 12:31:20 -0500

    admins can now edit individual property of a product

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -53,6 +53,10 @@ const typeDefs = gql`
     addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
     addProduct(name: String!, description: String!, price:Float!, quantity:Int!, category:ID!): Product
     editProduct(_id:ID!,name: String!, description: String!, price:Float!, quantity:Int!): Product
+    editProductName(_id:ID!, name: String!): Product
+    editProductDescription(_id:ID!, description: String!): Product
+    editProductPrice(_id:ID!, price:Float!): Product
+    editProductQuantity(_id:ID!, quantity:Int!): Product
     deleteProduct(_id:ID!): Product
     addOrder(products: [ID]!): Order
     updateUser(firstName: String, lastName: String, email: String, password: String): User

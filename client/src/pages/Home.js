9e84db1f9d31ccfb0e9b2dac6f8d279ebcf1721b
Diana Vu <aznalive@hotmail.com>
2022-11-28 11:05:04 -0500

    some changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ProductList from '../components/ProductList'
 import { Banner } from '../components/Banner'
 import { Testing } from "../components/Testing"
-import { FaMoon, FaSun } from 'react-icons/fa'
+
 
 const style = {
   hw: {
@@ -11,27 +11,8 @@ const style = {
 }
 
 const Home = () => {
-  // const [darkMode, setDarkMode] = useState(false)
 
   return (
-    // <div
-    //   className={
-    //     darkMode ? 'app-container dark-mode' : 'app-container light-mode'
-    //   }
-    // >
-    //   <div className='theme-switch'>
-    //     {!darkMode ? (
-    //       <span onClick={() => setDarkMode(!darkMode)}>
-    //         <FaMoon className='fa-icon' />
-    //       </span>
-    //     ) : (
-    //       <span onClick={() => setDarkMode(!darkMode)}>
-    //         <FaSun className='fa-icon' />
-    //       </span>
-    //     )}
-    //   </div>
-
-    // </div>
     <div style={style.hw}>
 
       <Banner />
